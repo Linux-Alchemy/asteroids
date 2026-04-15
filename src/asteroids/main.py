44adec118a07009db13cc47d1ccf204629ebbc35
rw42 <roguewizard42@gmail.com>
2026-04-15 11:09:30 -0400

    add player mechanix | add circle class

--- src/asteroids/main.py
+++ src/asteroids/main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -12,7 +17,10 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 
