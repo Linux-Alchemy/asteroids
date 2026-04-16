159325a3972939e45deee0d3637b8c8f603994b1
rw42 <roguewizard42@gmail.com>
2026-04-16 14:59:41 -0400

    add player speed | add forward/back movement | refactor screen update in main loop

--- src/asteroids/main.py
+++ src/asteroids/main.py
@@ -5,10 +5,15 @@ from player import Player
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
 
     while True:
         log_state()
@@ -17,8 +22,10 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
